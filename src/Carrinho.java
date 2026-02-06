d1dbfd57d87d13c0236fd68f644ada09fc25d2f3
matheusldl <matheuslacaudaluz2004@gmail.com>
2026-02-05 21:57:51 -0300

    Criação do codigo 1.0

--- src/Carrinho.java
+++ src/Carrinho.java
@@ -1,2 +1,44 @@
+import java.util.ArrayList;
+
 public class Carrinho {
+    private ArrayList<ProdutoJogo> carrinho = new ArrayList<>();
+    private double totalCarrinho = 0;
+
+    public double total(double resultado){
+        for (ProdutoJogo p: carrinho) {
+            resultado += p.getPreco();
+        }
+        aplicarDesconto(resultado);
+        return resultado;
+
+    }
+
+    private boolean podeAplicarDesconto(double total){
+       return total > 150;
+    }
+
+    private double aplicarDesconto(double total){
+        if (podeAplicarDesconto(total)) {
+            return total = (total - (total * 0.15));
+        }
+        else return total;
+    }
+
+
+
+    public ArrayList<ProdutoJogo> getCarrinho() {
+        return carrinho;
+    }
+
+    public void setCarrinho(ArrayList<ProdutoJogo> carrinho) {
+        carrinho = carrinho;
+    }
+
+    public double getTotalCarrinho() {
+        return totalCarrinho;
+    }
+
+    public void setTotalCarrinho(double totalCarrinho) {
+        this.totalCarrinho = totalCarrinho;
+    }
 }
--- src/ProdutoJogo.java
+++ src/ProdutoJogo.java
@@ -1,2 +1,25 @@
 public class ProdutoJogo {
+    private String nome;
+    private Double preco;
+
+    public ProdutoJogo(String nome, Double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
 }
