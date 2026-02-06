4373a0bc2bab761d408eedf0e02a8dacdb301f46
matheusldl <matheuslacaudaluz2004@gmail.com>
2026-02-05 22:24:52 -0300

    Começar Criação do Swing e Rezar pro listener funcionar

--- src/Hud.java
+++ src/Hud.java
@@ -1,3 +1,28 @@
-public class Hud {
+import javax.swing.*;
+import java.awt.*;
+
+public class Hud extends JFrame {
+
+
+    public Hud(){
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(850, 650);
+        add(janela());
+        setVisible(true);
+    }
+
+
+    private JPanel janela(){
+        JPanel janela = new JPanel();
+        janela.setBackground(Color.black);
+        return janela;
+    }
+
+
+    private JButton BotaoAdicionar(){
+
+        return BotaoAdicionar();
+    }
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ProdutoJogo jogo = new ProdutoJogo("Arata", 12.99);
-        Carrinho carrinho = new Carrinho();
-
+        Hud start = new Hud();
 
     }
 }
