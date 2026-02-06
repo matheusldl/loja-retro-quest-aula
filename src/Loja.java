f5f0c3be8b3d7a97dfc8756210bba6715157b057
matheusldl <matheuslacaudaluz2004@gmail.com>
2026-02-06 10:56:18 -0300

    Projeto Finalizado - Cadastrado os jogos

--- src/Loja.java
+++ src/Loja.java
@@ -5,9 +5,9 @@ public class Loja {
      private final ArrayList<ProdutoJogo> jogosVenda = new ArrayList<>();
 
     public Loja() {
-        jogosVenda.add(new ProdutoJogo("Arata", 11.99, "src/fotosJogo/aventura.png"));
-        jogosVenda.add(new ProdutoJogo("Arata", 22.99, "src/fotosJogo/aventura.png"));
-        jogosVenda.add(new ProdutoJogo("Arata", 33.99, "src/fotosJogo/aventura.png"));
+        jogosVenda.add(new ProdutoJogo("Super Aventura 64", 45.00, "src/fotosJogo/aventura.png"));
+        jogosVenda.add(new ProdutoJogo("Combate Espacial", 60.00, "src/fotosJogo/espacial.png"));
+        jogosVenda.add(new ProdutoJogo("Corrida Turbo", 50.00, "src/fotosJogo/corrida.png"));
     }
 
     public ArrayList<ProdutoJogo> getJogosVenda() {
